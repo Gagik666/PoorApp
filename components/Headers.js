e1b205669f90fd9f7a44017fd18c84c28fd79c7d
Gagik666 <gagikvardanyan6666@gmail.com>
2022-09-20 12:06:21 +0400

    add signOuth

--- components/Headers.js
+++ components/Headers.js
@@ -1,44 +1,51 @@
-import { View, StyleSheet } from "react-native";
-import React from "react";
-import { Entypo } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { firebase } from "../config";
+import { useNavigation } from "@react-navigation/native";
 
 export const Headers = () => {
-    return (
-        <View style = {styles.container}>
-            <Entypo
-                name="menu"
-                size={24}
-                color="black"
-            />
+  const navigation = useNavigation();
+  const auth = () => {
+    console.log(firebase.auth().currentUser);
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        if (firebase.auth().currentUser === null) {
+          navigation.navigate("login");
+        }
+      });
+  };
 
-            <View style = {{flexDirection: 'row'}}>
-                <Ionicons
-                name="notifications"
-                size={24}
-                color="#d9864b" />
-                <View style = {styles.viewNote}>
-                </View>
-            </View>
-            
-                
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => auth()}>
+        <Entypo name="menu" size={24} color="black" />
+      </TouchableOpacity>
+
+      <View style={{ flexDirection: "row" }}>
+        <Ionicons name="notifications" size={24} color="#d9864b" />
+        <View style={styles.viewNote}></View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        paddingTop: 45,
-        paddingHorizontal: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    viewNote: {
-        backgroundColor: "red",
-        height: 6,
-        width: 6,
-        borderRadius: 50,
-        marginLeft: -7
-    }
-})
+  container: {
+    width: "100%",
+    paddingTop: 45,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  viewNote: {
+    backgroundColor: "red",
+    height: 6,
+    width: 6,
+    borderRadius: 50,
+    marginLeft: -7,
+  },
+});
--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 export const SplashScreen = ({ navigation }) => {
+    
     setTimeout(function () { navigation.navigate("login") }, 3000)
     return (
         <View style={styles.container}>
